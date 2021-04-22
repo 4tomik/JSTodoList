48078d68810635da3f87239316e81094ff85a47d
Alvaro Lara <alvaro1542@gmail.com>
2021-04-22 17:16:01 -0400

    creacion index.js
    prueba 1

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const title = document.getElementById('tittle');
+    const description = document.getElementById('description');
+    const btn = document.getElementById('add');
+
+
+})
